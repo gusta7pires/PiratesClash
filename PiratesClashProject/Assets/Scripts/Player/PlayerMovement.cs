c3c93d62b912f17329159383d4df4fafeed4c271
Gustavo <gpires94@gmail.com>
2023-02-05 11:03:15 -0300

    Collisions and sprites

--- PiratesClashProject/Assets/Scripts/Player/PlayerMovement.cs
+++ PiratesClashProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,9 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float maxSpeed = 10f;
     [SerializeField] private float turnSpeed = 100f;
 
+    [SerializeField] private float collisorRadius = 0.1f;
+    [SerializeField] private Transform frontColl;
+    [SerializeField] private Transform backColl;
+
     private Vector2 movement;
     private Rigidbody2D rb;
 
+    public LayerMask groundLayers;
 
     private void Start(){
         rb = GetComponent<Rigidbody2D>();
@@ -27,6 +32,28 @@ public class PlayerMovement : MonoBehaviour
     private void PlayerInputs(){
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        CheckColliders();
+    }
+
+    private void CheckColliders(){
+        Collider2D bowCollider = Physics2D.OverlapCircle(frontColl.position, collisorRadius, groundLayers);
+        if (bowCollider!=null){
+            movement.x = movement.x/100;
+            movement.y = movement.y/100;
+        }
+
+        Collider2D sternCollider = Physics2D.OverlapCircle(backColl.position, collisorRadius, groundLayers);
+        if (sternCollider!=null){
+            movement.x = movement.x/100;
+            movement.y = movement.y/100;
+        }
+    }
+
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(new Vector3(frontColl.position.x, frontColl.position.y, 0f), collisorRadius);
+        Gizmos.DrawWireSphere(new Vector3(backColl.position.x, backColl.position.y, 0f), collisorRadius);
     }
 
     private void MoveFoward(){
